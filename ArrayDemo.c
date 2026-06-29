7d3b4f0d58c71980422db1db7a1a7520e0cc30da
Tejas Shah <tejasshah2k19@gmail.com>
2026-06-29 17:55:58 +0530

    array

--- ArrayDemo.c
+++ ArrayDemo.c
@@ -7,19 +7,49 @@ void insertItem(int location, int data)
 {
     // 5.45
 
-    for(int i=SIZE-1;i>=location-1;i--){
-        arr[i] = arr[i-1];
+    if (linearSearch(data) == 1)
+    {
+        printf("\nElement already inserted : %d ", data);
     }
-    arr[location-1] = data; 
+    else
+    {
+        for (int i = SIZE - 1; i >= location - 1; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+        arr[location - 1] = data;
+    }
+}
 
+int linearSearch(int item)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (arr[i] == item)
+        {
+            return 1; // true - found
+        }
+    }
+    return 0; // false -not found
 }
 
 void display()
 {
+    printf("\nArray Elements : ");
     for (int i = 0; i < SIZE; i++)
     {
         printf(" %d", arr[i]);
     }
+}
+
+void removeItem(int location)
+{
+    //
+    for (int i = location - 1; i < SIZE - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[SIZE - 1] = 0;
 }
 
 int main()
@@ -32,6 +62,7 @@ int main()
     insertItem(3, 50);
     insertItem(3, 60);
     display(); // 40	30	60	50	10	20
-
+    removeItem(1);
+    display(); // 30 60 50 10 20
     return 0;
 }
