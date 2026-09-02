ba436ab5415b23ceff2a05879453981396b2a942
Tejas Shah <tejasshah2k19@gmail.com>
2026-09-02 17:50:22 +0530

    linked list add last , add beg , display

--- LinkedListDemo.c
+++ LinkedListDemo.c
@@ -8,23 +8,62 @@ struct node
 };
 
 struct node *head = NULL;
+struct node *last = NULL;
 
-void addNode(int num)
+void addNode(int num) // 30
 {
     if (head == NULL)
     {
         // first insertion
-        head = (struct node*)malloc(sizeof(struct node));
+        head = (struct node *)malloc(sizeof(struct node));
         head->data = num;
-        head->next = NULL;  
+        head->next = NULL;
+        last = head;
     }
+    else
+    {
+        struct node *tmp = (struct node *)malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = NULL;
+        last->next = tmp;
+        last = tmp;
+    }
+}
+
+void display()
+{
+
+    struct node *p;
+    printf("\nLinkedList : ");
+
+    p = head;
+
+    while (p != NULL)
+    {
+        printf(" %d ", p->data); // 10 20 30 40
+        p = p->next;
+    }
+}
+
+void addNodeBeg(int num){
+    struct node *tmp = (struct node*)malloc(sizeof(struct node));
+
+    tmp->data= num; 
+    tmp->next = head; 
+    head = tmp; 
 }
 
 int main()
 {
 
     addNode(10);
-
-    printf(" %d ",head->data); //10 
+    addNode(20);
+    addNode(30);
+    display();//10 20 30 
+    addNode(40);
+    display();//10 20 30 40
+    
+    addNodeBeg(15);
+    display(); //15 10 20 30 40 
     return 0;
 }
