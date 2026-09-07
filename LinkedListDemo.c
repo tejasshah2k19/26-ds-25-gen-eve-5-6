45e8daaf48aafea5bcc3f1652c93ddb77dceae21
Tejas Shah <tejasshah2k19@gmail.com>
2026-09-07 17:53:25 +0530

    del beg , search , count , count odd , count even

--- LinkedListDemo.c
+++ LinkedListDemo.c
@@ -10,6 +10,8 @@ struct node
 struct node *head = NULL;
 struct node *last = NULL;
 
+
+//insertion --- at the end ---> 10->20->30->40
 void addNode(int num) // 30
 {
     if (head == NULL)
@@ -29,7 +31,9 @@ void addNode(int num) // 30
         last = tmp;
     }
 }
-
+//head -> next -> next -> next 
+//10     20 30 40   50 
+//head             last  
 void display()
 {
 
@@ -40,11 +44,14 @@ void display()
 
     while (p != NULL)
     {
-        printf(" %d ", p->data); // 10 20 30 40
+        printf(" %d ", p->data); // 10 20 30 40 50 
         p = p->next;
     }
 }
-
+//insertion - beg 
+//  10 20 30 40 
+//  50 
+//  50 10 20 30 40 
 void addNodeBeg(int num){
     struct node *tmp = (struct node*)malloc(sizeof(struct node));
 
@@ -52,6 +59,60 @@ void addNodeBeg(int num){
     tmp->next = head; 
     head = tmp; 
 }
+
+int countNode(){
+    int count=0;
+    struct node *p;
+
+    p = head ; 
+    while(p!=NULL){
+        count++;
+        p=p->next;
+    }
+
+    return count; 
+}
+//count all the odd node 
+int countOdd(){
+}
+
+//count all the even node 
+int countEven(){
+}
+
+
+int search(int data){
+    struct node *p = head; 
+    int found = 0; //not found 
+    while(p!=NULL){
+        if(p->data == data){
+            found  = 1; 
+            break;
+        }
+        p = p->next; 
+    }
+
+    if(found==0){
+        printf("\n%d not found",data);
+    }else{
+        printf("\n%d Found",data);
+    }
+
+    return found;//1 0 
+}
+//10 20 30 40 50
+//20 30 40 50 
+
+//15    10 20 30 40
+//head
+
+void delBeg(){
+    //head 
+    struct node *p = head; 
+    head = head->next; 
+    free(p);//
+}
+
 
 int main()
 {
@@ -65,5 +126,16 @@ int main()
     
     addNodeBeg(15);
     display(); //15 10 20 30 40 
+ 
+ 
+    printf("\nlength = %d",countNode());//return -> how many nodes we have  ? 
+
+    search(100); // not found 
+    search(30); // found 
+ 
+    //15 10 20 30 40 
+    delBeg();
+    display();//10 20 30 40 
+
     return 0;
 }
